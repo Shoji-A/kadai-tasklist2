32f0a6cbdc8c6ea6d65a38ba5de3fd0f36ba17e3
Shoji-A <z-sa@palette.plala.or.jp>
2019-10-30 20:57:35 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   root to: 'tasks#index'
   
-  resources :tasks
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :tasks
 end
